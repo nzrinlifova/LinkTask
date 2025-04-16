7f91cf161c8b1ea5c4738c58a52901332af5a906
Nezrin <nazrinna-bb208@code.edu.az>
2025-04-16 10:11:40 +0400

    Add project files.

---
+++ LinkTask/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+
+namespace LinkTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> links = new List<string>
+               {
+                 "https://www.youtube.com/watch?v=Wi7pzEYf4jE",
+                 "https://www.youtube.com/watch?v=Wi7pzEYf4jE",
+                 "https://www.youtube.com/watch?v=Wi7pzEYf4jE",
+                 "https://www.youtube.com/watch?v=Wi7pzEYf4jE",
+                 "https://www.youtube.com/watch?v=Wi7pzEYf4jE"
+               };
+
+            GetData(links).Wait();
+
+        }
+        static async Task GetData(List<string> links)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+
+                foreach (var link in links)
+                {
+                    try
+                    {
+                        HttpResponseMessage response = await client.GetAsync(link);
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            Console.WriteLine("Youtube video linki: " + link);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Gonderilen linkde problem var:  {link} { ex.Message}");
+                    }
+                }
+
+                stopwatch.Stop();
+
+                Console.WriteLine(stopwatch.Elapsed.TotalSeconds + " saniye");
+            }
+        }
+    }
+
+}
